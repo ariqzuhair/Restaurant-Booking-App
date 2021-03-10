2bbed8c7e76e1db976b6ea33ad20652c57be458a
LAPTOP-TF1536HM\Owner <daniishwafri1@gmail.com>
2021-03-10 15:30:17 +0800

    book form

--- app/src/main/java/com/example/fasttable/BookingForm.java
+++ app/src/main/java/com/example/fasttable/BookingForm.java
@@ -3,11 +3,10 @@ package com.example.fasttable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class BookNow extends AppCompatActivity {
+public class BookingForm extends AppCompatActivity {
 
     private EditText Name;
     private EditText NumberofPax;
@@ -19,23 +18,15 @@ public class BookNow extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_now);
+        setContentView(R.layout.activity_booking_form);
         getSupportActionBar().setTitle("Book Your Table Now");
+
 
         Name                = (EditText)findViewById(R.id.pt_name);
         NumberofPax         = (EditText)findViewById(R.id.tv_numpax);
         ReservationForm     = (Button)findViewById(R.id.btn_reserve);
         Date                = (EditText) findViewById(R.id.tv_date);
         Time                = (EditText) findViewById(R.id.tv_time);
-
-
-        ReservationForm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
-
-
 
 
     }
