7abca3a188d54c57df25f592d5262e6d6e430cf5
SENTINEL\ariqz <ariqzuhair4@gmail.com>
2021-03-14 22:08:11 +0800

    Latest Alpha Test

---
+++ README.md
@@ -0,0 +1,4 @@
+# group4
+
+#6 Time fail adding the databsse, still trying to wrap things up T_T
+#Continue with the rest of progress
---
+++ app/src/main/java/com/example/fasttable/Register.java
@@ -0,0 +1,191 @@
+package com.example.fasttable;
+
+import android.content.Intent;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+public class Register extends AppCompatActivity {
+
+    private EditText userName, userPassword, userEmail, userAge;
+    private Button regButton;
+    private TextView userLogin;
+    private FirebaseAuth firebaseAuth;
+    private ImageView userProfilePic;
+    private FirebaseStorage firebaseStorage;
+    private static int PICK_IMAGE = 123;
+    private StorageReference storageReference;
+
+    String email, name, age, password;
+    Uri imagePath;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null){
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imagePath);
+                userProfilePic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().setTitle("Register");
+        setupUIViews();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        userProfilePic.setImageResource(R.drawable.addpic);
+        userProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"select image"),PICK_IMAGE);
+            }
+        });
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validate())
+                {
+                    //upload data to database
+                    String user_email = userEmail.getText().toString().trim();
+                    String user_password = userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                sendEmailVerification();
+                            }else
+                            {
+                                Toast.makeText(Register.this,"Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this,Login.class));
+            }
+        });
+    }
+
+    private void setupUIViews()
+    {
+        userEmail      = (EditText)findViewById(R.id.reg_userEmail);
+        userName       = (EditText)findViewById(R.id.reg_userName);
+        userPassword   = (EditText)findViewById(R.id.reg_userPassword);
+        regButton      = (Button)findViewById(R.id.btn_regSignUp);
+        userLogin      = (TextView)findViewById(R.id.tv_userLogin);
+        userAge        = (EditText)findViewById(R.id.reg_age);
+        userProfilePic = (ImageView)findViewById(R.id.reg_userProfilePic);
+    }
+
+    private Boolean validate()
+    {
+        Boolean result = false;
+
+        password       = userPassword.getText().toString();
+        email          = userEmail.getText().toString();
+        age            = userAge.getText().toString();
+
+        if(name.isEmpty() || password.isEmpty() || email.isEmpty() || age.isEmpty() || imagePath == null)
+        {
+            Toast.makeText(this,"Please enter all the details", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private void sendEmailVerification() {
+        FirebaseUser firebaseUser = firebaseAuth.getInstance().getCurrentUser();
+        if(firebaseUser != null)
+        {
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()) {
+                        sendUserData();
+                        Toast.makeText(Register.this,"Successfully registered, Verification email has been sent!", Toast.LENGTH_SHORT).show();
+                        firebaseAuth.signOut();
+                        finish();
+                        startActivity(new Intent(Register.this,Login.class));
+                    }else
+                    {
+                        Toast.makeText(Register.this, "Verification mail has not been sent!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserData(){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = firebaseDatabase.getReference("User Info").child(firebaseAuth.getUid());
+        StorageReference imageReference = storageReference.child(firebaseAuth.getUid()).child("Profile Image");
+        UploadTask uploadTask = imageReference.putFile(imagePath);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Register.this,"Upload Failed", Toast.LENGTH_SHORT).show();
+            }
+
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(Register.this,"Upload Successful", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        UserProfile userProfile = new UserProfile(age, email, name);
+        myRef.setValue(userProfile);
+    }
+}
